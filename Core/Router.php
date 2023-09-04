12da30ded5aacc3ec2c1d3c2235031618f3e7ed6
David Lozano <david.loza010@gmail.com>
2023-09-04 11:59:46 -0400

    refactor(refactored login validation): created LoginForm class to hold logic for validating use cred
    created LoginForm class to hold logic for validating use credentials

--- Core/Router.php
+++ Core/Router.php
@@ -50,8 +50,8 @@
 			foreach ($this->routes as $route){
 				if ($route['uri'] === $uri && $route['method'] === strtoupper($method)){
 					Middleware::resolve($route['middleware']);
-					
-					return require base_path($route['controller']);
+//					dd(base_path('Http/controllers/'.$route['controller']));
+					return require base_path('Http/controllers/'.$route['controller']);
 				}
 			}
 			
--- Http/controllers/notes/store.php
+++ Http/controllers/notes/store.php
@@ -22,7 +22,7 @@
 
 	$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
 		'body' => $_POST['body'],
-		'user_id' => 1
+		'user_id' => 8
 	]);
 	
 	header('location: /notes');
